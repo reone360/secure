c31de363c687189cbbc1f980bd3bc9f92cfc3972
reone360 <spyros360reo@gmail.com>
2017-01-24 15:23:26 +0200

    Updated to handle comments

--- calls/index_call.php
+++ calls/index_call.php
@@ -20,4 +20,84 @@ class indexCall
     }
 
 
+    public function insertComments() //stores comments to db
+    {
+        //error_reporting(0); //Uncomment this to hide notice reports on Live server, else leave it commented for develop
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $db = "frameusers";
+        $table = "comments";
+
+        $commentTxt = $_POST['cmt'];
+        $user = $_SESSION['username'];
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $db);
+
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        else if (!(isset($_SESSION['username'])))
+        {
+            echo "Please login first";
+        }
+        else
+        {
+            $sql = "INSERT INTO $table (comment, username)VALUES ('$commentTxt','$user')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Comment Added";
+            } else {
+                //echo "Error: " . $sql . "<br>" . $conn->error;
+                echo "Comment was not added, if the problem persists please contact us";
+            }
+
+            $conn->close();
+            //echo "Connected successfully";
+            echo "<meta http-equiv=\"refresh\" content=\"1;url=http://localhost/secure/\" />";
+        }
+
+    }
+
+
+    public function callComments() //calls the comments to page
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $db = "frameusers";
+        $table = "comments";
+
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $db);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            $sql = "SELECT username, comment FROM $table";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows  > 0)
+            {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo $row["username"]. " - Commented: " . $row["comment"]. "<br>";
+                }
+
+            } else {
+                echo "0 results";
+            }
+            $conn->close();
+        }
+    }
+
+
+
 }
