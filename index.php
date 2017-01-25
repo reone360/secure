263531d7f5f68656c5f68def32d958850b440a16
reone360 <spyros360reo@gmail.com>
2017-01-25 12:59:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
 <?php
 echo"bs";
-
-$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'],'/')) : '/';
-
-if ($url == '/')
-{
-    header('Location: scenes/index_scene.php');
-}
