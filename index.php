fd10f6aab1e9a5afbaeb3fe7a8d5dde23fd96d54
reone360 <spyros360reo@gmail.com>
2017-01-25 13:00:32 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-echo"bs";
+
+$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'],'/')) : '/';
+
+if ($url == '/')
+{
+    header('Location: scenes/index_scene.php');
+}
